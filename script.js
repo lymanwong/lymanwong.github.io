e9e107bc312200a1053f101bc63bf3aaeae15bd7
lyman wong <lymanwong@gmail.com>
2015-04-29 14:56:59 -0700

    add toggle show hide

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ $(document).ready(function() {
   // $('.boom').click(function() {
   //   $('.wrapperbottom').fadeIn('slow');
   // });
-$('.boom').click(function(){
-  $('.wrapperbottom').fadeIn('slow');
-});
+  $('.boom').click(function(){
+    $('.wrapperbottom').fadeIn('slow');
+  });
+
+  $('.lilist').hide();
+  $('.li').click(function(){
+    $('.lilist').toggle('slow');
+  });
+
+  $('.gitlist').hide();
+  $('.git').click(function(){
+    $('.gitlist').toggle('slow');
+  });
+
+  $('.projectlist').hide();
+  $('.projects').click(function(){
+    $('.projectlist').toggle('slow');
+  });
+
 });
