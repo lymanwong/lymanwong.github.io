e8a8b84d46e0ebc20428c01e42c296b629590907
lyman wong <lymanwong@gmail.com>
2015-05-20 21:19:00 -0700

    adding sounds

--- script.js
+++ script.js
@@ -7,23 +7,7 @@ function playSomeSound(genre){
   });
 }
 
-window.onload = function(){
-  SC.initialize({
-    client_id: 'ef10d229673c01a84693af525e10e933'
-  });
-
-  var menuLinks = document.getElementsByClassName('genre');
-  for(var i=0; i<menuLinks.length; i++) {
-    var menuLink = menuLinks[i];
-    menuLink.onclick = function(e) {
-      e.preventDefault();
-      playSomeSound(menuLink.innerHTML);
-    };
-  };
-};
-
 $(document).ready(function() {
-
   $('.wrapperbottom').hide();
   $('.boom').click(function(){
     $('.wrapperbottom').toggle('slow');
@@ -55,3 +39,18 @@ $(document).ready(function() {
   });
 
 });
+
+window.onload = function(){
+  SC.initialize({
+    client_id: 'ef10d229673c01a84693af525e10e933'
+  });
+
+  var menuLinks = document.getElementsByClassName('genre');
+  for(var i=0; i<menuLinks.length; i++) {
+    var menuLink = menuLinks[i];
+    menuLink.onclick = function(e) {
+      e.preventDefault();
+      playSomeSound(menuLink.innerHTML);
+    };
+  };
+};
