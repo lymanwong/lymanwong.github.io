806cb593c49af1d2339c76ea1973e1943b88566c
lyman wong <lymanwong@gmail.com>
2015-11-12 10:36:17 -0600

    undone

--- inc/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <p>Copyright &copy; Lyman Wong 2015</p>
-            </div>
-        </div>
-    </div>
-</footer>
---
+++ script.js
@@ -0,0 +1,62 @@
+function playSomeSound(genre){
+  SC.get('/tracks', {
+    genres: genre
+  }, function(tracks){
+    var random = Math.floor(Math.random()*49);
+  SC.oEmbed(tracks[random].uri, {auto_play:true}, document.getElementById('player'));
+  });
+}
+
+window.onload = function(){
+  SC.initialize({
+    client_id: '8252b473b6d9a433882f050f2fa37842'
+  });
+
+  var menuLinks = document.getElementsByClassName('genre');
+  for(var i=0; i<menuLinks.length; i++) {
+    var menuLink = menuLinks[i];
+    menuLink.onclick = function(e) {
+      e.preventDefault();
+      playSomeSound(menuLink.innerHTML);
+    };
+  };
+};
+
+$(document).ready(function() {
+  $('.wrapperbottom').hide();
+  $('.boom').click(function(){
+    $('.wrapperbottom').toggle('slow');
+  });
+
+  $('.dbcpic').hide();
+  $('.dbc').click(function(){
+    $('.dbcpic').toggle('slow');
+  });
+
+  $('.lilist').hide();
+  $('.li').click(function(){
+    $('.lilist').toggle('slow');
+  });
+
+  $('.gitlist').hide();
+  $('.git').click(function(){
+    $('.gitlist').toggle('slow');
+  });
+
+  $('.musiclist').hide();
+  $('.music').click(function(){
+    $('.musiclist').toggle('slow');
+  });
+
+  $('.bloglist').hide();
+  $('.blog').click(function(){
+    $('.bloglist').toggle('slow');
+  });
+
+  $('.projectlist').hide();
+  $('.projects').click(function(){
+    $('.projectlist').toggle('slow');
+  });
+});
+
+
