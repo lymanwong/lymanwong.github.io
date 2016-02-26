c7fd80f274f48df09073448ffa837f08cc0875f8
lyman wong <lymanwong@gmail.com>
2016-02-25 16:05:53 -0800

    update

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    $('#topNav').affix({
+        offset: {
+            top: 200
+        }
+    });
+
+    new WOW().init();
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+
+    $('form').on('submit',function(event){
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'send_message.php',
+            data: $('form').serialize(),
+            success: function () {
+                alert('Success!');
+            }
+        });
+    });
+})(jQuery);
