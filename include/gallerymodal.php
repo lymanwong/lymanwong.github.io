5ec932aefdfc8f001b6536c9df3e6794ffe4cf7f
lyman wong <lymanwong@gmail.com>
2017-11-18 17:28:46 -0800

    added index

--- include/gallerymodal.php
+++ include/gallerymodal.php
@@ -1,16 +1,16 @@
-    <div id="galleryModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-body">
-                <img src="//placehold.it/1200x700/222?text=..." id="galleryImage" class="img-responsive centering_modal_image" />
-                <p>
-                    <br/>
-                    <div class="row" style="text-align: center;">
-                        <a href="" id="mylink" target="_blank"><button class="btn btn-primary btn-lg" role="button" aria-hidden="true" style="display:inline-block;">Take me there</button></a>
-                        <button class="btn btn-primary btn-lg" style="display:inline-block;" data-dismiss="modal" aria-hidden="true">Close <i class="ion-android-close"></i></button>
-                    </div>
-                </p>
-            </div>
-        </div>
+<div id="galleryModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-body">
+            <img src="//placehold.it/1200x700/222?text=..." id="galleryImage" class="img-responsive centering_modal_image" />
+            <p>
+                <br/>
+                <div class="row" style="text-align: center;">
+                    <a href="" id="mylink" target="_blank"><button class="btn btn-primary btn-lg" role="button" aria-hidden="true" style="display:inline-block;">Take me there</button></a>
+                    <button class="btn btn-primary btn-lg" style="display:inline-block;" data-dismiss="modal" aria-hidden="true">Close <i class="ion-android-close"></i></button>
+                </div>
+            </p>
         </div>
     </div>
+    </div>
+</div>
