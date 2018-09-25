e3da891dee5e9d2e8356be8838ba925afe6c1429
lyman wong <lymanwong@gmail.com>
2018-09-24 18:54:06 -0700

    updated experience

--- include/history.php
+++ include/history.php
@@ -3,23 +3,34 @@
         <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
             <h2 class="text-center text-primary">Experience</h2>
             <hr>
-            <div class="media wow fadeInRight">
-                <h3>LeEco US - May 2016 to Current</h3>
-                <div class="media-body media-middle">
-                    <p>Web Developer</p>
+            <div class="media wow fadeInLeft">
+                <h3>Udacity - January 2018 - current</h3>
+                <div class="media-left">
+                    <i class="icon-lg ion-cube"></i>
                 </div>
-                <div class="media-right">
-                    <i class="icon-lg ion-monitor"></i>
+                <div class="media-body media-middle">
+                    <p>Student Experience Specialist</p>
                 </div>
             </div>
             <hr>
+            <div class="media wow fadeInRight">
+                <h3>LeEco US - May 2016 - July 2017</h3>
+                <div class="media-body media-middle">
+                    <p>Web Developer</br>
+                    Social Media Community Manager
+                    </p>
+                </div>
+                <div class="media-right">
+                    <i class="icon-lg ion-android-folder-open"></i>
+                </div>
+            </div>
             <div class="media wow fadeInLeft">
                 <h3>Phlint Retail Analytics - Oct. 2015 to Apr. 2016</h3>
                 <div class="media-left">
                     <i class="icon-lg ion-monitor"></i>
                 </div>
                 <div class="media-body media-middle">
-                    <p>Front End Developer</p>
+                    <p>Front End Developer Intern</p>
                 </div>
             </div>
             <hr>
@@ -42,8 +53,8 @@
                     <p>Associate Community Manager, Community Specialist, and Game Lead</p>
                 </div>
             </div>
-            <hr>
-            <div class="media wow fadeInRight">
+            <!-- <hr> -->
+<!--             <div class="media wow fadeInRight">
                 <h3>Various, Inc. - Jan. 2007 to Apr. 2010</h3>
                 <div class="media-body media-middle">
                     <p>Project Manager</p>
@@ -51,7 +62,7 @@
                 <div class="media-right">
                     <i class="icon-lg ion-android-folder-open"></i>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
     </aside>
