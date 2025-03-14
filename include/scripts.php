c5bce33f15773ad0e3414db35efd6c61bf62f7aa
Lyman Wong <lymanwong@gmail.com>
2025-03-14 16:53:06 -0700

    updated contact link

--- include/scripts.php
+++ include/scripts.php
@@ -3,10 +3,9 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
-<!--     <script src="//code.angularjs.org/1.2.0rc1/angular-route.js"></script>
-    <script src="//code.angularjs.org/1.2.0rc1/angular-animate.js"></script> -->
+    <script src="//code.angularjs.org/1.2.0rc1/angular-route.js"></script>
+    <script src="//code.angularjs.org/1.2.0rc1/angular-animate.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
-    <script src="http://connect.soundcloud.com/sdk.js"></script>
     <script src="//cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
     <script src="js/scripts.js"></script>
     <script src="js/sound.js"></script>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="generator" content="Codeply">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.1/animate.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
     <link rel="stylesheet" href="css/style.css" />
